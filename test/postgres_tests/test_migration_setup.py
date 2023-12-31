b1f421ea30722c5efc650d7dc38ce6d247fcff5b
Harsh Tiwari <harshtiwari@Harshs-MacBook-Air.local>
2023-12-31 12:50:54 +0530

    Updated  workflow 18.0

--- test/postgres_tests/test_migration_setup.py
+++ test/postgres_tests/test_migration_setup.py
@@ -1,17 +1,16 @@
 import os
 import shutil
 import unittest
-from simple_migrator.utils import migration_tools
-from simple_migrator.utils.cli import setup_cli, setup_migrator
+from simple_migrator.utils.cli import setup_migrator
 from simple_migrator.utils.constants import *
 
 
 class TestingMigrationSetup(unittest.TestCase):
 
-    def test_setup_mysql_default_db_name(self):
+    def test_setup_default_db_name(self):
         os.environ.setdefault(
                 DATABASE_ENV_NAME_DEFAULT,
-                "mysql://root:Julia%401984@localhost:3306/test_db")
+                "mysql://root:root@localhost:3306/test_db")
         migration_tool = setup_migrator(None, database_env_name=None)
         self.assertTrue(os.path.exists(
                 os.path.join(
@@ -23,11 +22,11 @@ class TestingMigrationSetup(unittest.TestCase):
         if os.path.exists(MIGRATIONS_FOLDER_NAME):
             shutil.rmtree(MIGRATIONS_FOLDER_NAME)
 
-    def test_setup_mysql_custom_db_name(self):
+    def test_setup_custom_db_name(self):
         db_env_name = "DB_ENV_NAME"
         os.environ.setdefault(
                 db_env_name,
-                "mysql://root:Julia%401984@localhost:3306/test_db")
+                "mysql://root:root@localhost:3306/test_db")
         migration_tool = setup_migrator(None, database_env_name=None)
         self.assertTrue(os.path.exists(
                 os.path.join(
